5e2ff46feed793353c2bb3b089221ca622a400fc
quanbv <quanbv@reactplus.jp>
2022-11-25 10:57:02 +0700

    dynamic data

--- 03-ReactBasics_Components/src/components/ExpenseItem.js
+++ 03-ReactBasics_Components/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 11, 20);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 2854.44;
+
   return (
     <div className='expense-item'>
-      <div>November 25th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
