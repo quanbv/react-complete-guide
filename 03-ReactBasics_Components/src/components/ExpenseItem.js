bd0e2dfeee4c1e46e1803e4eb3f85485b29437b1
quanbv <quanbv@reactplus.jp>
2022-11-25 14:35:49 +0700

    add child component ExpenseDate

--- 03-ReactBasics_Components/src/components/ExpenseItem.js
+++ 03-ReactBasics_Components/src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('vi', { month: 'long'});
-  const day = props.date.toLocaleString('vi', { day: '2-digit'});
-  const year = props.date.getFullYear();
-
 
   return (
     <div className='expense-item'>
       {/* <div>{props.date.toISOString()}</div> */}
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.expenseTitle}</h2>
         <div className='expense-item__price'>${props.amount}</div>
