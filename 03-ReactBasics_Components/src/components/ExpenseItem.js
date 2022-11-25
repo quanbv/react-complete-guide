b8a61d1a94a69d93db8cf3ecf0067b462cd174ba
quanbv <quanbv@reactplus.jp>
2022-11-25 13:52:42 +0700

    add js logic to component

--- 03-ReactBasics_Components/src/components/ExpenseItem.js
+++ 03-ReactBasics_Components/src/components/ExpenseItem.js
@@ -1,16 +1,22 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 11, 20);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 2854.44;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('vi', { month: 'long'});
+  const day = props.date.toLocaleString('vi', { day: '2-digit'});
+  const year = props.date.getFullYear();
+
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.expenseTitle}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
