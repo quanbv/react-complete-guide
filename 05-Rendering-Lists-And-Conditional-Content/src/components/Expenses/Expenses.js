070033105a137283e81cedeb1c7b03217a7f5940
quanbv <quanbv@reactplus.jp>
2022-11-30 10:45:31 +0700

    map array and conditions

--- 05-Rendering-Lists-And-Conditional-Content/src/components/Expenses/Expenses.js
+++ 05-Rendering-Lists-And-Conditional-Content/src/components/Expenses/Expenses.js
@@ -12,18 +12,53 @@ const Expenses = (props) => {
     setFilterYear(selectedYear);
   }
 
+  const filteredExpenses = props.items.filter(expense => {
+    return filteredYear === 'all' ? expense.date.getFullYear().toString() !== expense :
+            expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((item) => (
+                        <ExpenseItem
+                          key={item.id}
+                          title={item.title}
+                          amount={item.amount}
+                          date={item.date}
+                        />
+                      ));
+  }
+
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear}
-        onChangeFilter={filterChangeHandler} />
-      {props.items.map((item) => 
-        <ExpenseItem
-                key={item.id}
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-               />
-      )}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* {filteredExpenses.length === 0 ? (
+        <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((item) => 
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+          )
+        )} */}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 && (
+          filteredExpenses.map((item) => 
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+          )
+        )} */}
+        {expensesContent}
     </Card>
   );
 }
