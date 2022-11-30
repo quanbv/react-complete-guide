3750dcdd6654c2542d306548cb3f2b91d5837e88
quanbv <quanbv@reactplus.jp>
2022-11-30 11:04:04 +0700

    break const JSX to new child component

--- 05-Rendering-Lists-And-Conditional-Content/src/components/Expenses/Expenses.js
+++ 05-Rendering-Lists-And-Conditional-Content/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilterYear] = useState('2020');
@@ -17,49 +18,16 @@ const Expenses = (props) => {
             expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((item) => (
-                        <ExpenseItem
-                          key={item.id}
-                          title={item.title}
-                          amount={item.amount}
-                          date={item.date}
-                        />
-                      ));
-  }
-
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      {/* {filteredExpenses.length === 0 ? (
-        <p>No expenses found.</p>
-        ) : (
-          filteredExpenses.map((item) => 
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-          )
-        )} */}
-        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length > 0 && (
-          filteredExpenses.map((item) => 
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-          )
-        )} */}
-        {expensesContent}
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+          <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </li>
   );
 }
 
